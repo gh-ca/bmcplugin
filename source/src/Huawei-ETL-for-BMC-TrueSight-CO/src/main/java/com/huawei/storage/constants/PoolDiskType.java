8eb7c15bbb0a2413673e2e082f21b4d63212c22b
chenjie <jie.chen@gh-ca.com>
2022-03-09 10:14:06 +0800

    存储池硬盘类型判断

--- source/src/Huawei-ETL-for-BMC-TrueSight-CO/src/main/java/com/huawei/storage/constants/PoolDiskType.java
+++ source/src/Huawei-ETL-for-BMC-TrueSight-CO/src/main/java/com/huawei/storage/constants/PoolDiskType.java
@@ -10,31 +10,31 @@ import java.util.Map;
  */
 public enum PoolDiskType {
 
-    SSD(0),
+//    SSD(0),
+//
+//    SAS(1),
+//
+//    NL_SAS(2);
 
+
+    FC(0),
     SAS(1),
+    SATA(2),
+    SSD(3),
+    NL_SAS(4),
+    SLC_SSD(5),
+    MLC_SSD(6),
+    FC_SED(7),
+    SAS_SED(8),
+    SATA_SED(9),
+    SSD_SED(10),
+    NL_SAS_SED(11),
+    SLC_SSD_SED(12),
+    MLC_SSD_SED(13),
+    NVMe_SSD(14),
+    NVMe_SSD_SED(16),
+    SCM_SSD(17);
 
-    NL_SAS(2);
-
-    /**
-     *     FC(0),
-     *     SAS(1),
-     *     SATA(2),
-     *     SSD(3),
-     *     NL_SAS(4),
-     *     SLC_SSD(5),
-     *     MLC_SSD(6),
-     *     FC_SED(7),
-     *     SAS_SED(8),
-     *     SATA_SED(9),
-     *     SSD_SED(10),
-     *     NL_SAS_SED(11),
-     *     SLC_SSD_SED(12),
-     *     MLC_SSD_SED(13),
-     *     NVMe_SSD(14),
-     *     NVMe_SSD_SED(16),
-     *     SCM_SSD(17);
-     */
 
 
     private int type;
